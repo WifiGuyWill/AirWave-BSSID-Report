648f8db722fba348280d79826f8181bfecb9ae8f
Will Smith <43626141+WifiGuyWill@users.noreply.github.com>
2022-02-24 09:13:32 -0800

    Update bssid.py
    Added support for csv, created global option to pick formate (csv of pdf).

--- bssid.py
+++ bssid.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #(c) 2022 Will Smith - WILL@WIFI-GUYS.COM
-#Thank You and Credits to: github.com/michaelrosejr https://github.com/Pack3tL0ss gitlab.com/ehalbwachs/
 
 """Connect to Airwave REST API to get AP BSSID, Create PDF then Send as Email Attachement"""
 
@@ -9,6 +8,7 @@
 import xml.etree.ElementTree as ET # libxml2 and libxslt
 import requests                    # HTTP requests
 from fpdf import FPDF              # Create PDF
+import pandas as pd                # Creat CSV
 import urllib3                     # Supress SSL Errors
 import smtplib, ssl                # Send Email
 
@@ -17,9 +17,13 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+
 # ---------------------------------------------------------------------------
 # Constants
 # ---------------------------------------------------------------------------
+
+# Set csv or pdf
+list_type = 'pdf'
 
 # Email Parameters
 gmail_user = 'eamil@gmail.com'
@@ -32,7 +36,6 @@ PASSWD = 'airwave_PW!'
 
 # URL for REST API
 LOGIN_URL = 'https://airwave.your-company.com/LOGIN'
-## may add later AP_LIST_URL = 'https://airwave.wifi-guys.com/ap_list.xml'
 AP_BSSID_URL = 'https://airwave.your-company.com/api/ap_bssid_list.xml'
 
 
@@ -43,12 +46,13 @@ HEADERS = {
 }
 
 # Misc
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) #Disable SSL Warnings
 
 # ---------------------------------------------------------------------------
-# Fonctions
+# Funtions
 # ---------------------------------------------------------------------------
 
+# AirWave Authentication
 def open_session():
     """Open HTTPS session with login"""
 
@@ -57,6 +61,7 @@ def open_session():
     loginamp = ampsession.post(LOGIN_URL, headers=HEADERS, data=data, verify=False)
     return {'session' : ampsession, 'login' : loginamp}
 
+# Gather AP BSSID Info
 def get_ap_bssid(session):
     """Get XML data and returns a dictionnaries list"""
     output = session.get(AP_BSSID_URL, headers=HEADERS, verify=False)
@@ -67,8 +72,8 @@ def get_ap_bssid(session):
     for ap in aps:
         for radio in ap.findall("radio"):
             bunch_of_bssids = [b.attrib.get("mac", "ERROR") for b in radio.findall("bssid")]
-            bssd_str = ' '.join(bunch_of_bssids)
-            bssid_data = ((f"{ap.attrib['name']} {bssd_str}"))
+            bssd_str = ','.join(bunch_of_bssids)
+            bssid_data = ((f"{ap.attrib['name']},{bssd_str}"))
             with open('bssid.txt', 'a') as f:
                 f.write(bssid_data + '\n')
 
@@ -81,22 +86,30 @@ class PDF(FPDF):
         self.cell(0, 10, 'Aruba AP BSSID List', 0, 1, 'C')
 
 def create_pdf():
+    with open('bssid.txt', 'r') as data:
+        plaintext = data.read()
+    plaintext = plaintext.replace(',', ' ')
     pdf = PDF()
     pdf.add_page()
     pdf.set_font("Arial", size = 12)
-    f = open("bssid.txt", "r")
-    for x in f:
-        pdf.cell(200, 8, txt = x, ln = 1, align = 'L')
+    pdf.multi_cell(200, 8, txt=plaintext, align = 'L')
     pdf.output("bssid.pdf")
+
+# Create CSV Files
+def create_csv():
+    df = pd.read_csv('bssid.txt',sep=';')
+    df.to_csv('bssid.csv', index=None)
+#    print(df.to_csv(index=None))
 
 # Removes data from temp file
 def cleanup():
     open("bssid.txt", "w").close()
 
+# Send Email (Gmail)
 def send_email():
     try:
         subject = "Aruba BSSID List"
-        body = "Please see PDF attachement."
+        body = "Please see attachement."
         sender_email = gmail_user 
         receiver_email = receiver_email_address
         password = gmail_password
@@ -109,8 +122,10 @@ def send_email():
 
         # Add body to email
         message.attach(MIMEText(body, "plain"))
-
-        filename = "bssid.pdf"  # In same directory as script
+        if list_type == "pdf":
+            filename = "bssid.pdf"
+        else:
+            filename = "bssid.csv"
 
         # Open PDF file in binary mode
         with open(filename, "rb") as attachment:
@@ -144,7 +159,10 @@ def send_email():
 def main():
     session = open_session()
     get_ap_bssid(session['session'])
-    create_pdf()
+    if list_type == "pdf":
+        create_pdf()
+    else:    
+        create_csv()
     send_email()
     cleanup()
 
